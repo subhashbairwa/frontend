f3d2840feb6df5195f85825624c518ae60a781ab
Subhash <120456241+subhashbairwa@users.noreply.github.com>
2025-03-23 21:10:37 +0530

    Initial commit

---
+++ vite/src/components/other/Header.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react'
+const Header = (props) => {
+
+  const [username, setUsername] = useState('')
+
+  useEffect(() => {
+    if(!props.data){
+      setUsername('Admin')
+    }else{
+      setUsername(props.data.firstName)
+    }
+  }, [props.data])
+
+  const logOutUser = () => {
+    localStorage.setItem('loggedInUser','')
+    props.changeUser('')
+    window.location.reload()
+  }
+
+  return (
+    <div className='flex items-end justify-between'>
+        <h1 className='text-2xl font-medium'>Hello <br /> <span className='text-3xl font-semibold'>{username}👋</span></h1>
+        <button onClick={logOutUser} className='bg-red-600 text-base font-medium text-white px-5 py-2 rounded-sm'>Log Out</button>
+    </div>
+  )
+}
+
+export default Header
